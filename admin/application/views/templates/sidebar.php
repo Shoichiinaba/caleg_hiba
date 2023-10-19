349114288443391e4ee27e76b731672b39f55009
Shoichiinaba <shoichiinaba01@gmail.com>
2023-10-19 08:20:37 +0700

    menambahkan menu keluar

--- admin/application/views/templates/sidebar.php
+++ admin/application/views/templates/sidebar.php
@@ -12,5 +12,11 @@
                 <span class="menu-title">Data Aspirasi</span>
             </a>
         </li>
+        <li class="nav-item">
+            <a class="nav-link" href="<?php echo site_url('../'); ?> ">
+                <i class="ti-share-alt menu-icon"></i>
+                <span class="menu-title">Keluar</span>
+            </a>
+        </li>
     </ul>
 </nav>
